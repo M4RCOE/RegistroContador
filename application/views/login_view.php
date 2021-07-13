d95b90ba1c6fcd683e4624fd13430ec81806250b
Jose Ricardo Baeza Candor <ricardobaezac10.24@gamil.com>
2021-07-13 10:45:47 -0500

    fifth commit

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -10,12 +10,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 	<title>Login</title>
 </head>
-<body>
-<div class="container" style="padding-top: 150px;">
-      <div class="row justify-content-center mt-5">
-        <div class="col-4 text-center">
-          <h2 class="text-center" >Inicio de sesión</h2>
-          <form action="<?php echo site_url('user_controller/login');?>" method="POST">
+<body class="d-flex flex-column min-vh-100 justify-content-center">
+<div class="container">
+      <div class="row justify-content-center">
+        <div class="col-4 text-center border border-3 border-dark pt-5 pb-5">
+          <h2 class="text-center mt-5" >Inicio de sesión</h2>
+          <form class="mb-5" action="<?php echo site_url('user_controller/login');?>" method="POST">
               <input type="text" name="username" class="form-control mt-4 mb-4" placeholder="Usuario">
               <input type="password" name="password" class="form-control mb-4" placeholder="Clave">            
               <input class="btn btn-dark" type="submit" value="Iniciar Sesión">            
